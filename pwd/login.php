05fc4ef415f67b516515e2abfd82b4651f5fbffa
Ahmad Rozin <43414218+L200170135@users.noreply.github.com>
2019-11-28 16:48:59 +0700

    Add files via upload

---
+++ pwd/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>TIP.id Login Page</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+	<link href='https://fonts.googleapis.com/css?family=Dekko' rel='stylesheet'>
+</head>
+<body>
+	<form class="box" action="index.html" method="post">
+		<h1>Login</h1>
+		<input type="text" name="" placeholder="Username">
+		<input type="password" name="" placeholder="Password">
+		<input type="submit" name="" value="Login">
+		<font face="Dekko" color="white">Belum punya akun? <a href="signup.php">Sign Up
+	</form>
+
+</body>
+</html>
